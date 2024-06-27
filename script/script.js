c34ccb422bfa5e85ded3fed66bcbf9d763ed0eb2
mendesx5 <bielmendes2208@gmail.com>
2024-06-26 22:07:20 -0300

    forth commit

--- script/script.js
+++ script/script.js
@@ -18,3 +18,14 @@ function rotateClockHand(element, rotation) {
 }
 
 setInterval(clockTick, 1000);
+
+window.onload = function () {
+    const video = document.getElementById('background-video');
+    const hourVideo = new date().getHours();
+
+    if (hourVideo > 6 && hourVideo < 18) {
+        video.src = 'day.mp4'
+    } else {
+        video.src = 'nigth.mp4'
+    }
+}
